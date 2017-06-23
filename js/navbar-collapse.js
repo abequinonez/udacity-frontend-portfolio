a3435fdb7d87eab62fe3addcf505bbd10eef53f5
Abraham Quinonez <abequinonez@users.noreply.github.com>
2017-06-22 17:46:37 -0700

    Updated navbar-collapse script

--- js/navbar-collapse.js
+++ js/navbar-collapse.js
@@ -1,10 +1,5 @@
-$(document).on("click", ".navbar-header", function(e) {
+$(document).on("click", ".navbar", function(e) {
 	if ($(e.target).is("a") && $(e.target).attr("class") != "dropdown-toggle") {
 		$(".navbar-collapse").collapse("hide");
-	}
-});
-$(document).on("click", ".navbar-collapse.in", function(e) {
-	if ($(e.target).is("a") && $(e.target).attr("class") != "dropdown-toggle") {
-		$(this).collapse("hide");
 	}
 });
